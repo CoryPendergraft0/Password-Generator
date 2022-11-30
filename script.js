c3c5223ab8bdd2e123548ea1a699a1026e1b72d7
Cory Pendergraft <cory.pendergraft81@gmail.com>
2022-11-30 17:15:13 -0500

    fixed several loop issues and figured out how to use the for loop

--- script.js
+++ script.js
@@ -8,13 +8,11 @@ var special = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
 var numbers = "0987654321";
 var upLetters = letters.toUpperCase();
 var sum = "";
-var length = prompt("Please choose between 8 and 128 characters")
-
-for (length; length < 8 || length > 128; ){
-  alert("Please select a valid number")
-  var length = prompt("Please choose between 8 and 128 characters")
-}
-
+var length = prompt("Please choose between 8 and 128 characters");
+  for (length; length < 8 || length > 128; ){
+    alert("Please select a valid number");
+      var length = prompt("Please choose between 8 and 128 characters");
+  }
 var isUpperCase = confirm("Would you like to use Uppercase?");
 var isLowerCase = confirm("Would you like to use Lowercase?");
 var isSpecial = confirm("Would you like to use Special characters?");
@@ -34,8 +32,9 @@ if (isNumber) {
   sum = sum.concat(numbers);
 } 
 
-if (!isNumber && !isLowerCase && !isUpperCase && isLowercase){
-  alert("You must choose at least 1 option")
+
+if (!isUpperCase && !isLowerCase && !isSpecial && !isNumber){
+  alert("You must choose at least 1 option");
 }
 }
 
